f04962a930328afc2916f7b85df503386b59e827
Jane Justiz <janejustiz@gmail.com>
2018-06-14 01:45:29 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,11 @@
 #write your code here
-
 def countdown(number)
   while number>0
     puts "#{number} SECOND(S)!"
     countdown_with_sleep(1)
     number-=1
   end
-  puts ""HAPPY NEW YEAR!""
+  puts "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(x)
